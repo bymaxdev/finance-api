04fdd57d894b616ce89fe227bdfc918e96d2c3d6
Max Reis <maxreisdossantos@hotmail.com>
2025-12-30 10:25:10 -0300

    Tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express from "express";
+import { router } from "./routes/route";
+import { errorHandlerMiddleware } from "./middlewares/error-handler";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use("/api", router);
+app.use(errorHandlerMiddleware);
+
+app.listen(PORT, () =>
+  console.log(`Server iniciado em http://localhost:${PORT}`)
+);
