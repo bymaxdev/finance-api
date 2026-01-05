bc2346ca188f7f7815c63c7cc14ef0e5288c99b7
Max Reis <maxreisdossantos@hotmail.com>
2026-01-05 12:21:44 -0300

    feat: implement account controller create and update handlers
    - Add create() handler to create new accounts
    - Add update() handler to modify existing accounts
    - Return proper HTTP status codes (201 for create)

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -18,4 +18,15 @@ export class AccontController {
     const accountById = await this.accountService.findById(id);
     res.json(accountById);
   };
+
+  create: Handler = async (req, res, next) => {
+    const newAccount = await this.accountService.create(req.body);
+    res.status(201).json(newAccount);
+  };
+
+  update: Handler = async (req, res, next) => {
+    const { id } = req.params;
+    const updateAccount = await this.accountService.update({ id, ...req.body });
+    res.json(updateAccount);
+  };
 }
